83c6d6a6eb88825177c731c39f0faf199022bdab
202100357 <202100357@post.au.dk>
2024-04-01 11:40:10 +1000

    changed namespace

--- Assets/MarianneFolder/Script/HumanManager.cs
+++ Assets/MarianneFolder/Script/HumanManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using UnityEngine;
 
-public class HumanManager : MonoBehaviour
+public class MariannesHumanManager : MonoBehaviour
 {
     public GameObject girl;
     public float value;
